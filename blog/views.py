c8c1061f7b7a4d10fb13850fcc95371d7d762f4a
Jeismu22 <j.rodriguezm21curnvirtual.edu.co>
2024-11-23 19:50:16 -0500

    gregasmo otros html y css

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+
+
+# Vista para listar los posts
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # Filtrar los posts que tienen una fecha de publicación pasada y ordenarlos
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # Pasar los posts al contexto de la plantilla
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
